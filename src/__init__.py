1db567be9d2b3f65cd5f11205b87dca356f533dc
donald323 <117969351+donald323@users.noreply.github.com>
2026-01-18 14:37:02 +0800

    Include Modifiers

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,11 @@ Flip 7 Game Simulation Framework
 Core modules for running Flip 7 card game simulations and tournaments.
 """
 
-from src.player import Player, Strategy
-from src.game_controller import GameController
-from src.environment import Flip7Environment
-from src.league_simulation import LeagueSimulation, LeaguePlayer
-from src.league_visualizer import LeagueVisualizer
+from .player import Player, Strategy
+from .game_controller import GameController
+from .environment import Flip7Environment
+from .league_simulation import LeagueSimulation, LeaguePlayer
+from .league_visualizer import LeagueVisualizer
 
 __all__ = [
     'Player',
